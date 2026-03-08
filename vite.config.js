19c25709dacd7ee8773ae5f8bebf7721278ed612
Sven-Erik Willrich <sven.willrich@googlemail.com>
2026-03-08 17:36:52 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mcda/',
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['svenwillrich.de'],
     watch: {
       usePolling: true,
       interval: 300
